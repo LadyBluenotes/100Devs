ea9793a8f83b7e0d2b147c55f9e89cb11ff41946
Sarah Gerrard <gerrardsarah@icloud.com>
2022-03-27 15:48:32 -0700

    JS Practice - Arrays

---
+++ JS Practice - In It To Win It/js/main.js
@@ -0,0 +1,23 @@
+// //Create an array of movies with at least three movies.
+
+// let threeMovies = ["Shrek", "The Adam Project", "The Matrix"];
+
+// // console.log(threeMovies)
+
+// //Using the array from above, store the first movie in a variable
+
+// // let firstMovie = threeMovies[0];
+
+// console.log(firstMovie);
+
+// // //Get the length of the original array and store it in a new variable
+
+// // let howManyMovies = threeMovies.length;
+
+// // console.log(howManyMovies);
+
+// // //Get the last element in that array and store it in a new variable. What if your array was really large and you didn't know the last index? Would your solution still work?
+
+// let lastMovie = threeMovies[threeMovies.length - 1]
+
+// console.log(lastMovie);
