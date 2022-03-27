94c0257c84e1ca758a9ea111400e983db1f73307
Sarah Gerrard <gerrardsarah@icloud.com>
2022-03-27 16:33:21 -0700

    JS Practice with Arrays

---
+++ JS Practice - Fight to the Finish/js/main.js
@@ -0,0 +1,25 @@
+//Create an array of movie titles. Loop through the array and each element to the h2.
+
+// let movieTitles = ['Shrek', ' Sherlock Holmes', ' Bring it On', ' Harry Potter', ' Lord of the Rings']
+
+// for (let i = 0; i < movieTitles.length; i++) {
+//     document.querySelector('h2').innerHTML += movieTitles[i]
+// }
+
+//Create an array of numbers. Loop through the array and add three to each number and replace the old number.
+
+// let numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// numberArray.forEach((item, index) => {
+//     numberArray[index] = item + 3
+// })
+
+//Find the average of all the numbers from question two.
+
+// let sum = 0
+
+// for (let i = 0; i < numberArray.length; i++) {
+//     sum = sum + numberArray[i]
+// }
+
+// console.log(sum / numberArray.length)
