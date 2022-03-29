87f0dbdae21e5481b49a7e3689539dcbb7efa04c
Sarah Gerrard <gerrardsarah@icloud.com>
2022-03-29 11:07:05 -0700

    updated JS pseudocode

--- HTML, CSS, JS Practice - ToDoList/js/main.js
+++ HTML, CSS, JS Practice - ToDoList/js/main.js
@@ -1,2 +1,18 @@
 import 'https://unpkg.com/open-props';
-import 'https://unpkg.com/open-props/normalize.min.css';
+import 'https://unpkg.com/open-props-css';
+
+
+// on click and enter key, add tasks written in input to the list, added to the dom
+
+// with pressing the trash can, remove tasks from list
+
+// upon pressing the checkbox, the task is completed
+
+// upon pressing the edit button, the task is editable
+
+// upon pressing the save or enter button, the task is saved
+
+// update pending tasks when a tasks is added or removed
+
+// clear all will remove all tasks from the list
+
