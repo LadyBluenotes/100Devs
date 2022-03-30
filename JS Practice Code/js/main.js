a4d98c53f77a07e0886be06ef645a6ec8fcc185e
Sarah Gerrard <gerrardsarah@icloud.com>
2022-03-30 09:36:16 -0700

    Modeling a bank account

---
+++ JS Practice Code/js/main.js
@@ -0,0 +1,24 @@
+/* Write a program that creates an account object with the following characteristic:
+  name = Alex, 
+  balance - 0, 
+  credit method adding the positive or negative value passed as an argument to the account balance,
+  a describe method returning the account description */
+
+// const account = {
+//   name: 'Alex',
+//   balance: 0,
+
+//   describe() {
+//     return `owner: ${account.name}, balance ${account.balance}`
+//   }
+// }
+
+// // console.log(account.describe())
+
+// //Credit 250
+// account.balance += 250
+
+// // //Debit 80
+// account.balance -= 80
+
+// console.log (account.describe())
