4324cb05929a25ca5d8f5aa82bc5817e0ef06120
Sarah Gerrard <98355961+gerrardsarah@users.noreply.github.com>
2022-04-07 16:49:47 -0700

    commented in previous line of code that was erased

--- JS Practice - Summer Games/js/main.js
+++ JS Practice - Summer Games/js/main.js
@@ -1,9 +1,16 @@
 //Create a function that takes in an array of numbers. Multiply each number together and alert the product. 
 
-function multiplyTheseNumbers(nums){
+function multiplyTheseNumbers(arr){
     let products = 1;
    arr.forEach(num => product *= num);
    alert(product);
+
+   // OR
+
+   // for(let i = 0; i < arr.length; i++){
+    //     products *= arr[i];
+    // }
+    // alert(products);
 }
 
 // products start at 1 so that the multiplication doesn't end up zeroing out.
